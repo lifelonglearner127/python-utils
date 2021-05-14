071c9d7251e21310b72b71dd34d218fb5c74dd78
Life-Long Learner <life.long.learner127@outlook.com>
2021-05-14 16:16:55 +0400

    added decorators

---
+++ decorators.py
@@ -0,0 +1,66 @@
+import functools
+import time
+
+
+def exec_time(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        value = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        run_time = end_time - start_time
+        print(f"Finished {func.__name__!r} in {run_time:.4f} secs")
+        return value
+
+    return wrapper
+
+
+def async_exec_time(func):
+    @functools.wraps(func)
+    async def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        value = await func(*args, **kwargs)
+        end_time = time.perf_counter()
+        run_time = end_time - start_time
+        print(f"Finished {func.__name__!r} in {run_time:.4f} secs")
+        return value
+
+    return wrapper
+
+
+def debug(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        args_repr = [repr(arg) for arg in args]
+        kwargs_repr = [f"{k}={v}" for k, v in kwargs.items()]
+        signature = ", ".join(args_repr + kwargs_repr)
+        print(f"Calling {func.__name__}({signature})")
+        value = func(*args, **kwargs)
+        print(f"{func.__name__!r} returned {value!r}")
+        return value
+
+    return wrapper
+
+
+def async_debug(func):
+    @functools.wraps(func)
+    async def wrapper(*args, **kwargs):
+        args_repr = [repr(arg) for arg in args]
+        kwargs_repr = [f"{k}={v}" for k, v in kwargs.items()]
+        signature = ", ".join(args_repr + kwargs_repr)
+        print(f"Calling {func.__name__}({signature})")
+        value = await func(*args, **kwargs)
+        print(f"{func.__name__!r} returned {value!r}")
+        return value
+
+    return wrapper
+
+
+def repeat(func_, *, n_times=2):
+    pass
+    # @functools.wraps(func)
+    # def wrapper(*args, **kwargs):
+    #     for _ in range(2):
+    #         value = func(*args, **kwargs)
+    #     return value
+    # return wrapper
